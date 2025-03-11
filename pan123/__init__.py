72cef3c3a3a95bd7eda3d6f685adca88eb5ad533
lixuehua2085 <102335781+dyfdannybeluga@users.noreply.github.com>
2025-03-11 11:47:52 +0800

    Update __init__.py
    新增了直连部分

--- pan123/__init__.py
+++ pan123/__init__.py
@@ -396,3 +396,47 @@ class Pan123:
         }
         r = requests.post(url, data=data, headers=self.header)
         return check_status_code(r)
+#直连部分 By-@狸雪花
+    def query_transcode(self, ids):#查询转码进度
+        url = self.base_url + "/api/v1/direct-link/queryTranscode"
+        data = {
+            "ids": ids
+        }
+        r = requests.post(url, data=data, headers=self.header)
+        return check_status_code(r)
+    def do_transcode(self, ids):#发起直连转码
+        url = self.base_url + "/api/v1/direct-link/doTranscode"
+        data = {
+            "ids": ids,
+        }
+        r = requests.post(url, data=data, headers=self.header)
+        return check_status_code(r)
+    def get_direct_link_m3u8(self, fileID):#获取转码直连
+        url = self.base_url + "/api/v1/direct-link/get/m3u8"
+        data = {
+            "fileID": fileID,
+        }
+        r = requests.post(url, data=data, headers=self.header)
+        return check_status_code(r)
+    def direct_link_enable(self, fileID):#开启直连功能
+        url = self.base_url + "/api/v1/direct-link/enable"
+        data = {
+            "fileID": fileID,
+        }
+        r = requests.post(url, data=data, headers=self.header)
+        return check_status_code(r)
+    def direct_link_disable(self, fileID):#关闭直连功能
+        url = self.base_url + "/api/v1/direct-link/disable"
+        data = {
+            "fileID": fileID,
+        }
+        r = requests.post(url, data=data, headers=self.header)
+        return check_status_code(r)
+    def direct_list_url(self, fileID):#获取直连地址
+        url = self.base_url + "/api/v1/direct-link/url"
+        data = {
+            "fileID": fileID,
+        }
+        r = requests.post(url, data=data, headers=self.header)
+        return check_status_code(r)
+#喵呜，直连部分写完了喵
