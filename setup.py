fbaa35f708d3f2601a0b965844581090529f56a0
Soda <soda_code@outlook.com>
2025-03-12 21:21:13 +0800

    更新 setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pan123",
-    version="0.1.1",
+    version="0.1.2",
     author="SodaCodeSave&lixuehua",
     author_email="soda_code@outlook.com",
-    description="This is an unofficial 123 Pan Open Platform API library, which can easily call the 123 Pan Open Platform in Python without the need to write repetitive code multiple times",
+    description="非官方的123云盘Python包",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/SodaCodeSave/Pan123",
