9d17084e953d9f42392202004eec41fcfe96a594
Soda <soda_code@outlook.com>
2025-03-24 23:56:48 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pan123",
-    version="0.1.4",
+    version="0.1.5",
     author="SodaCodeSave&lixuehua",
     author_email="soda_code@outlook.com",
     description="非官方的123云盘Python包",
