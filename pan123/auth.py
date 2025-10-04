fbb9ca3f8411f5144c290a1d3e587e61a35cc6ed
陨落基围虾 <3161880837@qq.com>
2025-10-04 21:54:25 +0800

    重构Auth类，优化get_access_token方法，移除dict_util模块

--- pan123/auth.py
+++ pan123/auth.py
@@ -1,20 +1,21 @@
-import json
 import requests
 
-from typing import Union
 from .utils.exceptions import ClientKeyError
-from .utils.dict_util import merge_dict
 from .utils.request import parse_response_data
+from .abstracts import Requestable
 
 
-def get_access_token(
-    client_id: str,
-    client_secret: str,
-    base_url: str = "https://open-api.123pan.com",
-    header: Union[dict, None] = None,
-) -> str:
-    header = header or {"Content-Type": "application/json", "Platform": "open_platform"}
-    url = base_url + "/api/v1/access_token"
-    data = {"clientID": client_id, "clientSecret": client_secret}
-    response = requests.post(url, data=data, headers=header)
-    return parse_response_data(response, ClientKeyError)["accessToken"]
+class Auth(Requestable):
+    def get_access_token(
+        self,
+        client_id: str,
+        client_secret: str,
+    ) -> str:
+        return parse_response_data(
+            requests.post(
+                self.use_url("/api/v1/access_token"),
+                data={"clientID": client_id, "clientSecret": client_secret},
+                headers=self.header,
+            ),
+            ClientKeyError,
+        )["accessToken"]
